403927f1ad421fe02c61458cbe793b7108597b72
Dallas James <dallasjames42@gmail.com>
2020-02-02 17:59:52 -0700

    more hosting stuff

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const auth = require("./auth/auth-router")
 const food = require("./foods/food-router")
 const dbConfig = require("./database/dbConfig")
 const port = process.env.PORT || 5000
-const host = process.env.HOST || "127.0.0.1"
+const host = process.env.HOST || "0.0.0.0"
 
 const sessionConfig = {
     name: "token",

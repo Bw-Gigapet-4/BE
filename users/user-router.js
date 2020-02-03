2c43a432081bfd5af44606fe03483a9a90a6a1d2
Dallas James <dallasjames42@gmail.com>
2020-02-02 19:06:57 -0700

    get users route

---
+++ users/user-router.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const restricted = require("../middleware/restricted")
+const db = require("./user-model")
+
+const router = express.Router()
+
+router.get("/users", restricted(), (req, res, next) => {
+    db.findBy()
+})

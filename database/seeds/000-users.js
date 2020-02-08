8f8646cbf730f0edef641fddba2effd595c72531
Dallas James <dallasjames42@gmail.com>
2020-02-08 11:28:12 -0700

    date format

--- database/seeds/000-users.js
+++ database/seeds/000-users.js
@@ -6,9 +6,9 @@ exports.seed = async (knex) => {
     { username: "Test", password: "somewhatSecurePass" }
   ])
   await knex("foods").insert([
-    { food: "strawberries", serving_size: 1, user_id: 1, category: "fruit", date: "2/7/2020" },
-    { food: "bananas", serving_size: 2, user_id: 1, category: "fruit", date: "2/7/2020" },
-    { food: "bread", serving_size: 3, user_id: 2, category: "starch", date: "2/7/2020" },
-    { food: "deer steak", serving_size: 4, user_id: 2, category: "meat", date: "2/7/2020" }
+    { food: "strawberries", serving_size: 1, user_id: 1, category: "fruit", date: "2020-02-08" },
+    { food: "bananas", serving_size: 2, user_id: 1, category: "fruit", date: "2020-02-08" },
+    { food: "bread", serving_size: 3, user_id: 2, category: "starch", date: "2020-02-08" },
+    { food: "deer steak", serving_size: 4, user_id: 2, category: "meat", date: "2020-02-08" }
   ])
 }
